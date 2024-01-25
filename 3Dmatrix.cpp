d6fe471dcfd835a2c586485061a70953cb92aca4
WayuOHm99 <121616763+WayuOHm99@users.noreply.github.com>
2024-01-25 15:39:10 +0700

    Update 3Dmatrix.cpp

--- 3Dmatrix.cpp
+++ 3Dmatrix.cpp
@@ -1,39 +1,38 @@
-/* LANG : C++ */
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main() {
-    int i, j, k, n, m, p;
+int main() { 
+    int i,j,k,n,m,p;
     string name[5][5][5];
-
+    
     cin >> n >> m >> p;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            for (k = 0; k < p; k++) {
-                cin >> name[i][j][k];
+        for(i=0;i<n;i++){
+            for(j=0;j<m;j++) {
+            	for(k=0;k<p;k++){				
+            	cin >> name[i][j][k];	
+				}
+            }
+        }
+   cout << "Elements in the matrix: " << endl;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            for(k=0;k<p;k++){
+                cout <<"name["<<i<<"]["<<j<<"]["<<k<<"]: ";
+                cout << name[i][j][k] << endl;
             }
         }
     }
-
-    cout << "Elements in The Martrix: " << endl;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
-            for (k = 0; k < p; k++) {
-                cout << "name[" << i << "][" << j << "][" << k << "]: " << name[i][j][k] << endl;
-            }
-        }
-    }
-
-    cout << "\nElements in The Martrix: " << endl;
-    for (i = 0; i < n; i++) {
-        cout << "name[" << i << "][][]: ";
-        for (j = 0; j < m; j++) {
-            for (k = 0; k < p; k++) {
-                cout << name[i][j][k] << " ";
+  cout << "\nElements in the matrix: "<< endl;
+    for (i=0;i<n;i++){
+        cout <<"name["<<i<<"][][]: ";
+        for(j=0;j<m;j++){
+            for(k=0;k<p;k++){
+                cout << name[i][j][k]<< " ";
             }
         }
         cout << endl;
     }
-
     return 0;
 }
